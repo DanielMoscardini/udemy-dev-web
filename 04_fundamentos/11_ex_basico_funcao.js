b95694147fccb9e60bad6c2d4e217b0c51b410cf
Daniel Bolonha Moscardini <moscardinibdaniel@gmail.com>
2023-02-07 11:56:15 -0300

    Iniciando exemplso de funcoes

---
+++ 04_fundamentos/11_ex_basico_funcao.js
@@ -0,0 +1,23 @@
+// funcao sem retorno.
+function imprimirSoma(n1, n2) {
+  console.log(n1 + n2);
+}
+
+imprimirSoma(2, 3); // 5
+imprimirSoma(2); // NaN
+imprimirSoma(2, 8, 9, 10); // 10
+imprimirSoma(); // NaN
+
+// funcao com retorno.
+function soma(a, b = 1) {
+  return a + b;
+}
+
+console.log(soma(2, 3)); // 5
+console.log(soma(2)); // 3
+
+let testeSemRetorno = imprimirSoma(2, 4);
+console.log(testeSemRetorno); // undefined
+
+let testeComRetorno = soma(2, 4);
+console.log(testeComRetorno); // 6
